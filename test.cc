6aa11656b3a8121967a7f25214eec8af9f207548
Florian Echtler <floe@butterbrot.org>
2013-11-15 16:51:31 +0100

    test alternative CRC implementation

--- test.cc
+++ test.cc
@@ -6,35 +6,33 @@ uint8_t testbuf[32] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 
 
 // see BT Core Spec 4.0, Section 6.B.3.1.1
-void myCrc(const uint8_t* data, uint8_t len, uint8_t* dst) {
+void mycrc( const uint8_t* buf, uint8_t len, uint8_t* dst ) {
 
-	uint8_t v, t, d;
+	// initialize 24-bit shift register in "wire bit order"
+	// dst[0] = bits 23-16, dst[1] = bits 15-8, dst[2] = bits 7-0
+	dst[0] = 0xAA;
+	dst[1] = 0xAA;
+	dst[2] = 0xAA;
 
 	while (len--) {
 
-		d = *data++;
-		for (v = 0; v < 8; v++, d >>= 1) {
+		uint8_t d = *(buf++);
 
-			// t = bit 23 (highest-value) 
-			t = dst[0] >> 7;
+		for (uint8_t i = 1; i; i <<= 1, d >>= 1) {
 
-			// left-shift the entire register by one
-			// (dst[0] = bits 23-16, dst[1] = bits 15-8, dst[2] = bits 7-0
-			dst[0] <<= 1;
-			if(dst[1] & 0x80) dst[0] |= 1;
-			dst[1] <<= 1;
-			if(dst[2] & 0x80) dst[1] |= 1;
-			dst[2] <<= 1;
+			// save bit 23 (highest-value), left-shift the entire register by one
+			uint8_t t = dst[0] & 0x01;         dst[0] >>= 1;
+			if (dst[1] & 0x01) dst[0] |= 0x80; dst[1] >>= 1;
+			if (dst[2] & 0x01) dst[1] |= 0x80; dst[2] >>= 1;
 
-			// if the bit just shifted out (former bit 23)
-			// and the incoming data bit are not equal:
-			// => bit_out ^ bit_in = 1
+			// if the bit just shifted out (former bit 23) and the incoming data
+			// bit are not equal (i.e. bit_out ^ bit_in == 1) => toggle tap bits
 			if (t != (d & 1)) {
-				// toggle register bits (=XOR with 1) according to CRC polynom
-				dst[2] ^= 0x5B; // 0b01011011 - x^6+x^4+x^3+x+1
-				dst[1] ^= 0x06; // 0b00000110 - x^10+x^9
+				// toggle register tap bits (=XOR with 1) according to CRC polynom
+				dst[2] ^= 0xDA; // 0b11011010 inv. = 0b01011011 ^= x^6+x^4+x^3+x+1
+				dst[1] ^= 0x60; // 0b01100000 inv. = 0b00000110 ^= x^10+x^9
 			}
-		}       
+		}
 	}
 }
 
@@ -104,4 +102,15 @@ int main() {
 	btLeWhiten(testbuf, 32, btLeWhitenStart(39));
 	
 	for (int i = 0; i < 32; i++) printf("%02hhx ",testbuf[i]); printf("\n");
+
+	uint8_t crc[3] = {0x55,0x55,0x55};
+	btLeCrc(testbuf, 24, crc);
+	for (uint8_t i = 0; i < 3; i++)
+		crc[i] = swapbits(crc[i]);
+
+	printf("CRC (orig): %02hhx %02hhx %02hhx\n",crc[0],crc[1],crc[2]);
+
+	uint8_t crc2[3];
+	mycrc(testbuf, 24, crc2);
+	printf("CRC (mine): %02hhx %02hhx %02hhx\n",crc2[0],crc2[1],crc2[2]);
 }
