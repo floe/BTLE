9e4055fd8c4469ce156b735fc9d208c70353c4c6
Florian Echtler <floe@butterbrot.org>
2014-02-22 14:27:12 +0100

    add example script for generating advertisement beacons with BlueZ on Linux

---
+++ bluez_adv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo hciconfig hci0 up
+sudo hciconfig hci0 leadv 3
+
+# 0x08       opcode group (LE)
+# 0x0008     opcode command (set LE adv data)
+
+# 0b         adv data length
+# 02 01 05   flags (LE-only device, non-connectable) 
+# 07 09 ...  name
+# 00 00 ...  padding (to 32 bytes including length)
+
+sudo hcitool -i hci0 cmd 0x08 0x0008  0b  02 01 05  07 09 66 6f 6f 62 61 7a  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
