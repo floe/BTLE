d90e0ba8aabe9257287869f1f11bfd17b975110a
Florian Echtler <floe@butterbrot.org>
2014-02-22 14:41:32 +0100

    add example for custom data

--- bluez_adv.sh
+++ bluez_adv.sh
@@ -6,12 +6,15 @@ sudo hciconfig hci0 up
 # enable non-connectable undirected advertisements
 sudo hciconfig hci0 leadv 3
 
+# send custom HCI command:
+
 # 0x08       opcode group (LE)
 # 0x0008     opcode command (set LE adv data)
 
-# 0b         adv data length
+# 0e         adv data length (should be at most 0x15 for compatibility with NRF24L01+)
 # 02 01 05   flags (LE-only device, non-connectable) 
 # 07 09 ...  name
+# 02 ff fe   custom data (02 length, ff type, fe data)
 # 00 00 ...  padding (to 32 bytes including length)
 
-sudo hcitool -i hci0 cmd 0x08 0x0008  0b  02 01 05  07 09 66 6f 6f 62 61 7a  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+sudo hcitool -i hci0 cmd 0x08 0x0008  0e  02 01 05  07 09 66 6f 6f 62 61 72  02 ff fe  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
