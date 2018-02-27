cf41c94803fa1ab215d0a4fc5bde42cd75e7ae35
Florian Echtler <floe@butterbrot.org>
2018-02-27 16:06:00 +0100

    fix the MF data & flags to be conformant

--- bluez_adv.sh
+++ bluez_adv.sh
@@ -31,8 +31,8 @@ sudo hcitool -i hci0 cmd 0x08 0x000A  01
 # set advertisement data (_after_ advertising is enabled)
 
 # 0e         adv data length (should be at most 0x15 for compatibility with NRF24L01+)
-# 02 01 05   flags (LE-only device, non-connectable) 
+# 02 01 06   flags (LE-only device, general discoverable mode) 
 # 07 09 ...  name
 # 02 ff fe   custom data (02 length, ff type, fe data)
 # 00 00 ...  padding (to 32 bytes including length)
-sudo hcitool -i hci0 cmd 0x08 0x0008  0e  02 01 05  07 09 66 6f 6f 62 61 72  02 ff fe  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+sudo hcitool -i hci0 cmd 0x08 0x0008  10  02 01 06  07 09 61 62 63 64 65 66  04 ff f1 0e fe  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
